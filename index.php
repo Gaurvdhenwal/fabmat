cb989444c17b2ebb30423f81ce27a6215a179e64
Gaurav Dhenwal <gauravdhenwal699@gmail.com>
2016-06-07 20:03:04 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="icon" type="image/png" href='LOGO/LOGO 1 PNG.png' />
+<title>
+	
+	FabMat
+
+</title>
+<link rel="stylesheet" href="css/bootstrap.css" />
+<link rel='stylesheet' href='font-awesome/css/font-awesome.css'/>
+<link rel='stylesheet' href="css/navigation.css" />
+<link rel="stylesheet" href="css/home.css"/>
+
+
+<body>
+
+
+
+<nav class="navbar navbar-default"  data-spy="affix" data-offset-top="50">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="fabmat.php"><img src="LOGO/logo_black.png" width="30" height="30" /> FABMAT</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        
+        
+        <li><a href="#">PRODUCTS</a></li>
+        <li><a href="#">CUSTOMERS</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">CONTACT</a></li>
+        <li ><a href="signin.php" style="color:#22b8eb !important;">SIGN IN</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!----------------------------------------------navigation over--------------------->
+
+<div class="jumbotron">
+	
+</div>
+
+
+</body>
+<script src="scripts/jquery.js"></script>
+<script src="scripts/bootstrap.js"></script>
+<script src="scripts/homenew.js"></script>
+</html>
